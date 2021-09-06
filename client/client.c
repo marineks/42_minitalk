b3fdb4977e06362310de7cf057d6ee09705c3b0c
Marine SANJUAN <marine@sanjuan.eu>
2021-09-06 19:01:49 +0200

    client can send a char to server which displays its binary conversion now

--- client/client.c
+++ client/client.c
@@ -6,24 +6,58 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 12:15:28 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/09/06 14:54:51 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/09/06 18:59:00 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
+void	convertCharToBits(int pid, char c)
+{
+	int bit_index;
+	bit_index = 7;
+	while (bit_index >= 0)
+	{
+		if (c & (1 << bit_index))
+		{
+			if (kill(pid, SIGUSR2) == SUCCESS)
+			{
+				write(1, "Signal '1' envoyé\n", 19);
+				usleep(100);
+			}
+			else
+				write(1, "KO.\n", 3);
+		}	
+		else
+		{
+			if (kill(pid, SIGUSR1) == SUCCESS)
+			{
+				write(1, "Signal '0' envoyé\n", 19);
+				usleep(100);
+			}
+			else
+				write(1, "KO.\n", 3);
+		}
+		bit_index--;
+	}
+}
+
+
 int main(int argc, char **argv) 
 {
 
-	if (argc == 2)
+	if (argc == 3)
 	{
 		int pid = ft_atoi(argv[1]);
 		// signal(SIGUSR1, reception_handler);
-		if (kill(pid, SIGUSR1) == SUCCESS)
-			write(1, "Signal envoyé\n", 15);
-		else
-			write(1, "KO.\n", 3);
-		// pause();
+		char *strToSend = argv[2];
+		int i = 0;
+
+		while (strToSend[i])
+		{
+			convertCharToBits(pid, strToSend[i]);
+			i++;
+		}
 	}
 	
 	return (0);
--- server/server.c
+++ server/server.c
@@ -6,34 +6,36 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 12:15:34 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/09/06 15:27:52 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/09/06 18:56:15 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
-void reception_handler()
+void reception_handler(int signal)
 {
-	// if (signal == SIGUSR1)
-		write(1, "Received SIGUSR1!\n", 18);
+	if (signal == SIGUSR2)
+		write(1, "1", 1);
+	else if (signal == SIGUSR1)
+		write(1, "0", 1);
 }
 
 int main(void)
 {
 	int pid = getpid();
+	struct sigaction sa;
+	sa.sa_handler = reception_handler;
 
 	write(1, "ID : ", 5);
 	ft_putnbr_dec(pid);
 	write(1, "\n", 1);
 	
-
 	while (1)
-	{
-		struct sigaction sa;
-		sa.sa_handler = reception_handler;
-
-		sigaction(SIGUSR1, &sa, NULL);
+	{	
+		if (sigaction(SIGUSR1, &sa, NULL) == ERROR)
+			write(1, "KO\n", 3);
+		if (sigaction(SIGUSR2, &sa, NULL) == ERROR)
+			write(1, "KO\n", 3);
 	}
-		
 	return (0);
 }
